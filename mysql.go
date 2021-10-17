bba81f1ab9db9be99f447eb4b19ecd55e63b42c0
Xue Jiao <jiao.xue@seamoney.com>
2021-10-17 20:34:59 +0800

    engine.sync the new struct to database, this method will auto add column, index, unique

--- mysql.go
+++ mysql.go
@@ -1,23 +1,67 @@
-// Copyright 2013 The XORM Authors. All rights reserved.
-// Use of this source code is governed by a BSD
-// license that can be found in the LICENSE file.
-
-// Package xorm provides is a simple and powerful ORM for Go. It makes your
-// database operation simple.
-
 package xorm
 
 import (
-	"fmt"
+	"crypto/tls"
+	//"fmt"
+	"regexp"
 	"strconv"
+	//"strings"
+	"time"
 )
 
 type mysql struct {
+	user              string
+	passwd            string
+	net               string
+	addr              string
+	dbname            string
+	params            map[string]string
+	loc               *time.Location
+	timeout           time.Duration
+	tls               *tls.Config
+	allowAllFiles     bool
+	allowOldPasswords bool
+	clientFoundRows   bool
+}
+
+/*func readBool(input string) (value bool, valid bool) {
+	switch input {
+	case "1", "true", "TRUE", "True":
+		return true, true
+	case "0", "false", "FALSE", "False":
+		return false, true
+	}
+
+	// Not a valid bool value
+	return
+}*/
+
+func (cfg *mysql) parseDSN(dsn string) (err error) {
+	//cfg.params = make(map[string]string)
+	dsnPattern := regexp.MustCompile(
+		`^(?:(?P<user>.*?)(?::(?P<passwd>.*))?@)?` + // [user[:password]@]
+			`(?:(?P<net>[^\(]*)(?:\((?P<addr>[^\)]*)\))?)?` + // [net[(addr)]]
+			`\/(?P<dbname>.*?)` + // /dbname
+			`(?:\?(?P<params>[^\?]*))?$`) // [?param1=value1&paramN=valueN]
+	matches := dsnPattern.FindStringSubmatch(dsn)
+	//tlsConfigRegister := make(map[string]*tls.Config)
+	names := dsnPattern.SubexpNames()
+
+	for i, match := range matches {
+		switch names[i] {
+		case "dbname":
+			cfg.dbname = match
+		}
+	}
+	return
+}
+
+func (db *mysql) Init(uri string) error {
+	return db.parseDSN(uri)
 }
 
 func (db *mysql) SqlType(c *Column) string {
 	var res string
-	fmt.Println("-----", c.Name, c.SQLType.Name, "-----")
 	switch t := c.SQLType.Name; t {
 	case Bool:
 		res = TinyInt
@@ -55,12 +99,12 @@ func (db *mysql) QuoteStr() string {
 	return "`"
 }
 
-func (db *mysql) AutoIncrStr() string {
-	return "AUTO_INCREMENT"
-}
-
 func (db *mysql) SupportEngine() bool {
 	return true
+}
+
+func (db *mysql) AutoIncrStr() string {
+	return "AUTO_INCREMENT"
 }
 
 func (db *mysql) SupportCharset() bool {
@@ -69,4 +113,23 @@ func (db *mysql) SupportCharset() bool {
 
 func (db *mysql) IndexOnTable() bool {
 	return true
+}
+
+func (db *mysql) IndexCheckSql(tableName, idxName string) (string, []interface{}) {
+	args := []interface{}{db.dbname, tableName, idxName}
+	sql := "SELECT `INDEX_NAME` FROM `INFORMATION_SCHEMA`.`STATISTICS`"
+	sql += " WHERE `TABLE_SCHEMA` = ? AND `TABLE_NAME` = ? AND `INDEX_NAME`=?"
+	return sql, args
+}
+
+func (db *mysql) ColumnCheckSql(tableName, colName string) (string, []interface{}) {
+	args := []interface{}{db.dbname, tableName, colName}
+	sql := "SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA` = ? AND `TABLE_NAME` = ? AND `COLUMN_NAME` = ?"
+	return sql, args
+}
+
+func (db *mysql) TableCheckSql(tableName string) (string, []interface{}) {
+	args := []interface{}{db.dbname, tableName}
+	sql := "SELECT `TABLE_NAME` from `INFORMATION_SCHEMA`.`TABLES` WHERE `TABLE_SCHEMA`=? and `TABLE_NAME`=?"
+	return sql, args
 }
--- sqlite3.go
+++ sqlite3.go
@@ -1,13 +1,10 @@
-// Copyright 2013 The XORM Authors. All rights reserved.
-// Use of this source code is governed by a BSD
-// license that can be found in the LICENSE file.
-
-// Package xorm provides is a simple and powerful ORM for Go. It makes your
-// database operation simple.
-
 package xorm
 
 type sqlite3 struct {
+}
+
+func (db *sqlite3) Init(uri string) error {
+	return nil
 }
 
 func (db *sqlite3) SqlType(c *Column) string {
@@ -56,4 +53,19 @@ func (db *sqlite3) SupportCharset() bool {
 
 func (db *sqlite3) IndexOnTable() bool {
 	return false
+}
+
+func (db *sqlite3) IndexCheckSql(tableName, idxName string) (string, []interface{}) {
+	args := []interface{}{idxName}
+	return "SELECT name FROM sqlite_master WHERE type='index' and name = ?", args
+}
+
+func (db *sqlite3) TableCheckSql(tableName string) (string, []interface{}) {
+	args := []interface{}{tableName}
+	return "SELECT name FROM sqlite_master WHERE type='table' and name = ?", args
+}
+
+func (db *sqlite3) ColumnCheckSql(tableName, colName string) (string, []interface{}) {
+	args := []interface{}{tableName}
+	return "SELECT name FROM sqlite_master WHERE type='table' and name = ? and sql like '%`" + colName + "`%'", args
 }
